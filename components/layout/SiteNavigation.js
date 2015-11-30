e736eecce81abc0b05e21f42d92b908acb2463d9
ejaxon <eric@deepweave.com>
2015-11-30 08:39:53 -0500

    Fixed to use bootstrap-sass, not bootstrap

--- components/layout/SiteNavigation.js
+++ components/layout/SiteNavigation.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import 'jquery';
-import bootstrap, {navbar, navs} from 'bootstrap';
+import bootstrap, {navbar, navs} from 'bootstrap-sass';
 
 class SiteNavigation extends Component {
 
