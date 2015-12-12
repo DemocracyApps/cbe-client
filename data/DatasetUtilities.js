645d8d17eefc1d2fcc3e241a9caf31a13763e793
ejaxon <eric@deepweave.com>
2015-12-12 12:02:01 -0500

    Wrote the differences transform

--- data/DatasetUtilities.js
+++ data/DatasetUtilities.js
@@ -16,8 +16,7 @@ class DatasetUtilities {
     }
 
     /* 
-     * This routine takes 2 arguments:
-     *      data - the raw dataset
+     * This routine takes 1 argument in addition to the data:
      *      detailLevel - the maximum depth to go to
      */
      rollupHierarchy (data, args) {
@@ -46,19 +45,25 @@ class DatasetUtilities {
     }
 
     /* 
-     * This routine takes only one argument:
-     *      data - the raw dataset
-     * If the input has N values, it outputs 2*N-1 values which are the original N
-     * plus the N-1 differences between successive pairs.
+     * This routine takes 1 argument in addition to the data:
+     *      useInfinity - Whether to use the infinity sign for new items or the word "New"
+     *
+     * If the input has N values, it outputs N-1 differents between successive pairs, N-1 percentage change
+     * entries (or N/A or New for negative items or items with previous value zero, respectively). It also
+     * outputs a 'percentSort' field based on the *last* difference.
      */
      differences (data, args) {
+        let useInfinity = args.useInfinity;
         let rows = [];
         let initValues = function(len, value) { let a = []; for (let i=0; i<len; ++i) a.push(value); return a; };
         data.map(function (row, index) {
             let len = row['values'].lenght;
             let current = {
                 categories: row['categories'].slice(0),
-                values: initValues(len, 0.0)
+                values: initValues(len, 0.0),
+                differences: initValues(len-1,0.0),
+                percents: initValues(len-1,""),
+                percentSort: 0
             };
 
             row['values'].forEach((item, index) => {
@@ -66,7 +71,30 @@ class DatasetUtilities {
             });
             if (len > 1) {
                 for (let i=1; i<len; ++i) {
-                    current['values'][len + i - 1] = current['values'][i] - current['values'][i-1];
+                    let cur = current['values'][i];
+                    let prev = current['values'][i-1];
+                    let diff = cur - prev;
+                    current['differences'][i - 1] = diff;
+                    current['percents'][i - 1] = current['values'][i] - current['values'][i-1];
+                    if (Math.abs(prev) < 0.001) {
+                        if (useInfinity) {
+                            current['percents'][i-1] = String.fromCharCode(8734) + " %";
+                        }
+                        else {
+                            current['percents'][i-1] = "New";
+                        }
+                        current['percentSort'] = 10000 * Math.abs(diff);
+                    }
+                    else if (cur < 0. || prev < 0.) {
+                        current['percents'][i-1] ="N/A";
+                        current['percentSort'] = 10000 * Math.abs(diff);
+                    }
+                    else {
+                        let pct = Math.round(1000*(diff)/prev)/10;
+                        if (pct > 0) pct = "+" + pct;
+                        current['percents'][i-1] = (pct) + "%";
+                        current['percentSort'] = Math.abs(current['percents'][i-1]);
+                    }
                 }
             }
             rows.push(current);
